5524a073e3eb3b8e3071d78e4cc5b4d426f2d53a
dv0c <tasosmeidanis12@gmail.com>
2023-05-21 12:27:30 +0300

    modified:   components/post-item.tsx

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -11,6 +11,8 @@ interface PostItemProps {
   mode?: string
 }
 
+// this is for github
+
 export function PostItem({ post, mode }: PostItemProps) {
   return (
     <div className="flex items-center justify-between p-4">
