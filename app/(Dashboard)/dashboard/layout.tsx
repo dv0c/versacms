4cd278ccee8b909835a0de12397f70b4c4f3a145
dv0c <tasosmeidanis12@gmail.com>
2023-05-27 13:07:09 +0300

    modified:   app/(Dashboard)/dashboard/layout.tsx

--- app/(Dashboard)/dashboard/layout.tsx
+++ app/(Dashboard)/dashboard/layout.tsx
@@ -2,18 +2,19 @@ import DashboardNav from "@/components/Dashboard-Nav";
 import SiteHeader from "@/components/Site-Header"
 import { SiteFooter } from "@/components/site-footer";
 import { dashboardConfig } from "@/config/dashboard";
+import { siteConfig } from "@/config/site";
 import { getCurrentUser } from "@/libs/session"
 import { notFound } from "next/navigation";
 
 export const metadata = {
-    title: 'Sparkle Press | Dashboard',
-    description: 'Sparkle Press Dashboard',
+    title: siteConfig.name,
+    description: siteConfig.name + ' Dashboard',
 }
 
 export default async function DashboardLayout({ children, }: { children: React.ReactNode }) {
 
     const user = await getCurrentUser();
-    
+
     if (!user) {
         return notFound()
     }
