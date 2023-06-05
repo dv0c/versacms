d4a34cca586955d03c9bbaa4a91b36a276f82f04
dv0c <tasosmeidanis12@gmail.com>
2023-06-05 09:17:35 +0300

    modified:   app/(Auth)/login/page.tsx

--- app/(Auth)/login/page.tsx
+++ app/(Auth)/login/page.tsx
@@ -33,5 +33,9 @@ export default async function Login() {
 }
 
 function FormFallback() {
-    return <></>
+    return <>
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+
+        </div>
+    </>
 }
