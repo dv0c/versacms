b6d8ed9b757a5c62349f1f66f4abc033eb908782
dv0c <tasosmeidanis12@gmail.com>
2023-06-06 08:57:02 +0300

    modified:   components/Dashboard-Nav.tsx

--- components/Dashboard-Nav.tsx
+++ components/Dashboard-Nav.tsx
@@ -24,7 +24,7 @@ const DashboardNav = (props: Props) => {
                 {props.items.map((i) => {
                     const Icon = Icons[i.icon || "arrowRight"]
                     return (
-                        <div key={i.title}>
+                        <div key={i.title} className={i.admin && props.user?.role !== 'administrator' ? 'hidden' : ''}>
                             {
                                 i.admin && props.user?.role !== 'administrator' ? null : <Link href={i.href || ""} key={i.title}>
                                     <span className={`${selectedRoute === i.href ? "bg-accent" : ""} group flex gap-2 items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground transparent`}>
